af9742acd86845823c51c6b937efe853168d6abe
Prince-Abir <Mahmudabir886@gmail.com>
2022-02-10 23:48:51 +0600

    Added Splash Activity,Payment Activity and Taost in RateUs CardView

--- app/src/main/java/com/app/musicplayer/MainActivity.java
+++ app/src/main/java/com/app/musicplayer/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -15,10 +16,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.musicplayer.AllSections.BanglaSong;
 import com.app.musicplayer.AllSections.HindiSong;
+import com.app.musicplayer.Payment.PaymentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout banglaSong, englishSong, hindiSong, arabicSongs;
+
+    ImageView PaymentIcon;
+
+    LinearLayout rateUsButton;
 
 
     private boolean isConnected() {
@@ -30,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         return networkInfo != null && networkInfo.isConnected();
     }
 
-    private void CustomDialog(){
+    private void CustomDialog() {
 
         new AlertDialog.Builder(MainActivity.this)
                 .setTitle("Network Error!")
@@ -56,7 +62,12 @@ public class MainActivity extends AppCompatActivity {
         hindiSong = findViewById(R.id.hindi_songs);
         arabicSongs = findViewById(R.id.arabic_songs);
 
+        PaymentIcon = findViewById(R.id.paymentIcon);
 
+        rateUsButton = findViewById(R.id.rate_us_button);
+
+
+        //Bangla Card View
         banglaSong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,30 +82,53 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        englishSong.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Under Development", Toast.LENGTH_SHORT).show();
-            }
-        });
+        //Hindi Card View
         hindiSong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 if (isConnected()) {
-                    startActivity(new Intent(MainActivity.this, BanglaSong.class));
+                    startActivity(new Intent(MainActivity.this, HindiSong.class));
                 } else {
                     CustomDialog();
                 }
             }
         });
-        arabicSongs.setOnClickListener(new View.OnClickListener() {
+
+        //English Card View
+        englishSong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Under Development", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Under Development\nWill be added in version 2.0", Toast.LENGTH_SHORT).show();
             }
         });
 
+
+        //Arabic Card View
+        arabicSongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Under Development\nWill be added in version 2.0", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        //Payment Section
+        PaymentIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PaymentActivity.class));
+            }
+        });
+
+
+        //Rate US Card View
+        rateUsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Under Development\nWill be added in version 2.0", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
